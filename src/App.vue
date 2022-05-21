ba34a24011d2a7c5dc997159f4dfae96fa9d3f21
Greg Brewton <greg@studio42dev.com>
2022-05-21 14:07:03 -0600

    Adds toggle to reminder

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
@@ -22,8 +22,13 @@ export default {
   },
   methods: {
     deleteTask(id) {
-      if(confirm('Delete task?'))
-      this.tasks = this.tasks.filter(t => t.id !== id)
+      if (confirm('Delete task?'))
+        this.tasks = this.tasks.filter(t => t.id !== id)
+    },
+    toggleReminder(id) {
+      this.tasks.forEach(t => {
+        if (t.id === id) t.reminder = !t.reminder
+      })
     }
   },
   created() {
