719caa6cb4edb73815e20dfb424c1d8e22853f3d
Greg Brewton <greg@studio42dev.com>
2022-05-21 12:32:51 -0600

    Adds button with color, title props

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button :style="{background: color}" class="btn" >{{title}}</button>
+</template> 
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        title: {
+            type: String,
+            default: "button",
+            required: true
+        },
+        color: {
+            type: String,
+            default: "blue",
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,23 @@
 <template>
     <header>
-        <h1>{{title}}</h1>
+        <h1>{{ title }}</h1>
+        <Button title="Add Task" color="green" />
     </header>
 </template>
 
 <script>
+import Button from "./Button.vue";
 export default {
     name: 'Header',
     props: {
         title: {
             type: String,
-            default: "Please provide a title",
+            default: "Title",
             required: true
         }
+    },
+    components: {
+        Button
     }
 }
 </script>
